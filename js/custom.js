f4995733063b04165548b9a3beda3ea3a8534bf2
mihyun9718 <algy9718@naver.com>
2024-11-18 16:38:08 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,12 +4,12 @@ $(function () {
     const tl = gsap.timeline();
 
     tl
-        .from('.intro h2', { x: -1000, opacity: 0, delay: 1 })
+        .from('.intro p', { x: -1000, opacity: 0, delay: 1 })
         .from('.intro p', { x: -1000, opacity: 0 })
         .from('.intro .star', { y: 200, opacity: 0, rotate: 720 })
 
     $('#portfolio').fullpage({
-        anchors: ['intro', 'pf01', 'pf02', 'pf03', 'profile'],
+        anchors: ['intro', 'pf01', 'pf02', 'pf03', 'pf04', 'pf05'],
         css3: false,
         onLeave: function (idx, nidx, dir) {
             console.log(idx, nidx, dir);
@@ -20,12 +20,16 @@ $(function () {
                 tl.restart();
             }
 
-            $('.num').text(nidx - 1);
+            $('.num').text;
 
             $('.zz_bg').css({
                 background: `url(./img/zz0${nidx - 1}.png) no-repeat center center/cover`
             })
+
+
+
         }
+
     });
 
     // $('#header .gnb li a').on('click', function () {
@@ -66,7 +70,7 @@ $(function () {
         motionPath: {
             path: `#path`,
             align: `#path`,
-            alignOrigin: [0., 0.7],
+            alignOrigin: [0.3, 0.7],
             autoRotate: true,
 
         },
