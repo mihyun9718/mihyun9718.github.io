309b5739dac6c731973f1c57d584fb279c5e94e7
mihyun9718 <algy9718@naver.com>
2024-12-16 02:10:44 +0900

    Add files via upload

--- js/custom.mini.js
+++ js/custom.mini.js
@@ -7,11 +7,11 @@ $(function () {
         motionPath: {
             path: `#path1`,
             align: `#path1`,
-            alignOrigin: [0.5, 0.5],
+            alignOrigin: [0.5, 0.7],
             autoRotate: true,
 
         },
-        duration: 5,
+        duration: 6,
         // delay: 5 * num,
     })
 })
